dfa49de1074d38505fb3ade3f65f4e6c0ef5513c
chenjiamanman <1194343027@qq.com>
2023-11-23 17:38:14 +0800

    Update basic.tsx
    Signed-off-by: chenjiamanman <1194343027@qq.com>

--- components/statistic/demo/basic.tsx
+++ components/statistic/demo/basic.tsx
@@ -4,16 +4,16 @@ import { Button, Col, Row, Statistic } from 'antd';
 const App: React.FC = () => (
   <Row gutter={16}>
     <Col span={12}>
-      <Statistic title="Active Users" value={112893} />
+      <Statistic title="Active Users" value={1128999999993} />
     </Col>
     <Col span={12}>
-      <Statistic title="Account Balance (CNY)" value={112893} precision={2} />
+      <Statistic title="Account Balance (CNY)" value={11289999999993} precision={2} />
       <Button style={{ marginTop: 16 }} type="primary">
         Recharge
       </Button>
     </Col>
     <Col span={12}>
-      <Statistic title="Active Users" value={112893} loading />
+      <Statistic title="Active Users" value={1128939999999999999999} loading />
     </Col>
   </Row>
 );
